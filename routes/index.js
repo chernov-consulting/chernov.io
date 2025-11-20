a860e3b3e998052cbe9cae1ee93d92ee9032e3e6
Anton Chernov <anton@chernov.io>
2025-11-20 15:47:48 +0000

    latest

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Chernov Consulting' });
+  res.render('home', { title: 'Chernov' });
 });
 
 module.exports = router;
